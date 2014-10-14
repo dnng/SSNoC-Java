384e2f3bcae88bb4036e53d1c4cec0a463bb3f58
Lloyd D'Silva <lloyddsilva@gmail.com>
2014-10-14 14:38:26 -0700

    merging measure memory changes

--- src/main/java/edu/cmu/sv/ws/ssnoc/rest/SSNoCAppInitializer.java
+++ src/main/java/edu/cmu/sv/ws/ssnoc/rest/SSNoCAppInitializer.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import edu.cmu.sv.ws.ssnoc.common.logging.Log;
+import edu.cmu.sv.ws.ssnoc.common.utils.MeasurementUtils;
 import edu.cmu.sv.ws.ssnoc.data.util.DBUtils;
 
 public class SSNoCAppInitializer extends HttpServlet {
@@ -17,10 +18,10 @@ public class SSNoCAppInitializer extends HttpServlet {
 		// of this method. This includes initializing database etc.
 		try {
 			DBUtils.initializeDatabase();
+			//MeasurementUtils.measure();
 		} catch (SQLException e) {
 			Log.error("Oops :( We ran into an error when trying to intialize "
 					+ "database. Please check the trace for more details.", e);
 		}
 	}
-
 }
