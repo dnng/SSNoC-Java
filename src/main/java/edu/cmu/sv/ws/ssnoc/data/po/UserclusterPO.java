fd0f44aefb38724bda72800fb1ed5a1c971ea9c0
Poorva Jain <poorva.jain@sv.cmu.edu>
2014-10-15 14:41:23 -0700

    Usergroups files

---
+++ src/main/java/edu/cmu/sv/ws/ssnoc/data/po/UserclusterPO.java
@@ -0,0 +1,118 @@
+package edu.cmu.sv.ws.ssnoc.data.po;
+
+import java.sql.Timestamp;
+
+import com.google.gson.Gson;
+
+/**
+ * This is the persistence class to save all user information in the system.
+ * This contains information like the user's name, his role, his account status
+ * and the password information entered by the user when signing up. <br/>
+ * Information is saved in SSN_USERS table.
+ * 
+ */
+public class UserclusterPO {
+	private long userId;
+	private String userName;
+	private String password;
+	private String salt;
+	private long lastStatusCrumbId;
+	private String lastStatus;
+	private long lastLocationCrumbId;
+	private String lastLocation;
+	private Timestamp lastStatusTime;
+	private Timestamp createdAt;
+	private Timestamp modifiedAt;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public long getLastStatusCrumbId() {
+		return lastStatusCrumbId;
+	}
+
+	public void setLastStatusCrumbId(long lastStatusCrumbId) {
+		this.lastStatusCrumbId = lastStatusCrumbId;
+	}
+
+	public String getLastStatus() {
+		return lastStatus;
+	}
+
+	public void setLastStatus(String lastStatus) {
+		this.lastStatus = lastStatus;
+	}
+
+	public long getLastLocationCrumbId() {
+		return lastLocationCrumbId;
+	}
+
+	public void setLastLocationCrumbId(long lastLocationCrumbId) {
+		this.lastLocationCrumbId = lastLocationCrumbId;
+	}
+	public String getLastLocation() {
+		return lastLocation;
+	}
+
+	public void setLastLocation(String lastLocation) {
+		this.lastLocation = lastLocation;
+	}
+
+	public Timestamp getLastStatusTime() {
+		return lastStatusTime;
+	}
+
+	public void setLastStatusTime(Timestamp lastStatusTime) {
+		this.lastStatusTime = lastStatusTime;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Timestamp getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(Timestamp modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
+	}
+
+}
---
+++ src/main/java/edu/cmu/sv/ws/ssnoc/rest/usergroupsService.java
@@ -0,0 +1,102 @@
+package edu.cmu.sv.ws.ssnoc.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlElementWrapper;
+
+import edu.cmu.sv.ws.ssnoc.common.logging.Log;
+import edu.cmu.sv.ws.ssnoc.common.utils.ConverterUtils;
+import edu.cmu.sv.ws.ssnoc.data.dao.DAOFactory;
+import edu.cmu.sv.ws.ssnoc.data.dao.ILocationCrumbDAO;
+import edu.cmu.sv.ws.ssnoc.data.dao.IMessageDAO;
+import edu.cmu.sv.ws.ssnoc.data.dao.IStatusCrumbDAO;
+import edu.cmu.sv.ws.ssnoc.data.dao.IUserDAO;
+import edu.cmu.sv.ws.ssnoc.data.po.LocationCrumbPO;
+import edu.cmu.sv.ws.ssnoc.data.po.MessagePO;
+import edu.cmu.sv.ws.ssnoc.data.po.StatusCrumbPO;
+import edu.cmu.sv.ws.ssnoc.data.po.UserPO;
+import edu.cmu.sv.ws.ssnoc.dto.Message;
+import edu.cmu.sv.ws.ssnoc.dto.StatusCrumb;
+import edu.cmu.sv.ws.ssnoc.dto.User;
+
+/**
+ * This class contains the implementation of the RESTful API calls made with
+ * respect to a message.
+ * 
+ */
+
+@Path("/usergroups")
+public class usergroupsService extends BaseService {
+	/**
+	 * This method loads up the users those have chatted with the specified user in the past
+	 * 
+	 * @return - array of all active users
+	 */
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@XmlElementWrapper(name = "usergroups")
+	@Path("/unconnected")
+
+	public List<User> loadChatBuddies(@PathParam("username") String userName) {
+		Log.enter();
+		
+		List<User> users = null;
+		try
+		{
+			List<UserPO> userPOs = DAOFactory.getInstance().getUserDAO().loadChatBuddies(userName);
+			
+			users = new ArrayList<User>();
+			for (UserPO po : userPOs) {
+				User dto = ConverterUtils.convert(po);
+				users.add(dto);
+			}
+			
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+		return users;
+	}
+	@Path("/users")
+	public class UsersService extends BaseService {
+		/**
+		 * This method loads all active users in the system.
+		 * 
+		 * @return - List of all active users.
+		 */
+		@GET
+		@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+		@XmlElementWrapper(name = "users")
+		public List<User> loadUsers() {
+			Log.enter();
+
+			List<User> users = null;
+			try {
+				List<UserPO> userPOs = DAOFactory.getInstance().getUserDAO().loadUsers();
+
+				users = new ArrayList<User>();
+				for (UserPO po : userPOs) {
+					User dto = ConverterUtils.convert(po);
+					users.add(dto);
+				}
+			} catch (Exception e) {
+				handleException(e);
+			} finally {
+				Log.exit(users);
+			}
+
+			return users;
+		}
+}
