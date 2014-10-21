9087cdc5bec612c6e1ed517d2e1276aa9bbaa4a6
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-21 00:19:41 -0700

    Remove trailing whitespace

--- src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/IMessageDAO.java
+++ src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/IMessageDAO.java
@@ -7,48 +7,48 @@ import edu.cmu.sv.ws.ssnoc.data.po.MessagePO;
 /**
  * Interface specifying the contract that all implementations will implement to
  * provide persistence of Status Crumb information in the system.
- * 
+ *
  */
 public interface IMessageDAO {
 	/**
 	 * This method will save the information of the wall message into the database.
-	 * 
+	 *
 	 * @param MessagePO
 	 *            - Status Crumb information to be saved.
 	 */
 	long saveWallMessage(MessagePO messagePO);
-	
+
 	/**
 	 * This method will save the information of the chat message into the database.
-	 * 
+	 *
 	 * @param MessagePO
 	 *            - Status Crumb information to be saved.
 	 */
 	long saveChatMessage(MessagePO messagePO);
-	
+
 	/**
 	 * This method will load all wall message in the
 	 * database.
-	 * 
+	 *
 	 * @return - List of all  wall messages.
 	 */
 	List<MessagePO> loadWallMessages();
-	
+
 
 	/**
 	 * This method will load all chat message in the
 	 * database.
-	 * 
+	 *
 	 * @return - List of all  chat messages.
 	 */
 	List<MessagePO> loadChatMessages();
-	
+
 	/**
 	 * This method will load a particular message by it's id.
-	 * 
+	 *
 	 * @param messageId
 	 *            - message id to search for.
-	 * 
+	 *
 	 * @return - messagePO.
 	 */
 
@@ -57,16 +57,16 @@ public interface IMessageDAO {
 
 	/**
 	 * load chat messages exchanged between two users
-	 * 
+	 *
 	 * @param userName1
 	 * 			- the sender
 	 * @param userName2
 	 * 			- the receiver
-	 * 
+	 *
 	 * @return - List of messages
 	 */
 	List<MessagePO> getAllChatMessagesForPeers(String userName1, String userName2);
-	
+
 	/**
 	 * save a new incoming chat message
 	 */
