3ba7a29189f6a7834186b49506e48e3f730e5029
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-21 00:19:18 -0700

    Add test methods for Message Service

--- src/it/java/edu/cmu/sv/ws/ssnoc/test/WallMessageTest.java
+++ src/it/java/edu/cmu/sv/ws/ssnoc/test/WallMessageTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.sv.ws.ssnoc.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Timestamp;
@@ -20,17 +21,20 @@ import edu.cmu.sv.ws.ssnoc.rest.UserService;
 
 public class WallMessageTest {
 
+	User user1 = new User();
+	User user2 = new User();
 	@Before
 	public void setUp() throws Exception {
-		User user = new User();
-		user.setUserName("foo");
-		user.setPassword("1234");
-		
 		UserService userService = new UserService();
-		userService.addUser(user);
-		
-		//Message msg = new Message();
-		
+
+		user1.setUserName("foo");
+		user1.setPassword("1234");
+
+		user2.setUserName("bar");
+		user2.setPassword("1234");
+
+		userService.addUser(user1);
+		userService.addUser(user2);
 	}
 
 	@After
@@ -39,15 +43,16 @@ public class WallMessageTest {
 
 	@Test
 	public void testSendMessageToPublicWall() {
+
 		Message msg = new Message();
 		msg.setAuthor("foo");
 		Timestamp postedAt = new Timestamp(1234);
 		msg.setPostedAt(postedAt);
-		msg.setContent("test test lloyd owes a me a beer");
-		
+		msg.setContent("testSendMessageToPublicWall");
+
 		MessageService msgService = new MessageService();
 		Response response = msgService.addWallMessage("foo", msg);
-		assertEquals(((Message)response.getEntity()).getContent(), "test test lloyd owes a me a beer");
+		assertEquals(((Message)response.getEntity()).getContent(), "testSendMessageToPublicWall");
 	}
 
 	@Test
@@ -55,19 +60,64 @@ public class WallMessageTest {
 		Message msg = new Message();
 		MessagesService msgsService = new MessagesService();
 		MessageService msgService = new MessageService();
-		System.out.println("t");
-		//Add atleast 1 message
+
+		/*
+		 * Add message with null usename
+		 */
+		assertNull(msgService.addWallMessage("", msg));
+
+		/*
+		 * Add atleast 1 message
+		 */
 		msg.setAuthor("foo");
 		Timestamp postedAt = new Timestamp(1234);
 		msg.setPostedAt(postedAt);
-		msg.setContent("test test lloyd owes a me a beer");
-		
-		Response response = msgService.addWallMessage("foo", msg);
-		
-		List<Message> messages = (List<Message>) msgsService.loadWallMessages();
+		msg.setContent("testGetAllMessagesFromPublicWall");
+
+		msgService.addWallMessage("foo", msg);
+
+		List<Message> messages = msgsService.loadWallMessages();
 		assertTrue(messages.size() != 0);
 		for (Message m : messages) {
 			assertTrue(m instanceof Message);
 		}
+	}
+
+	@Test
+	public void testLoadMessageInfoByGivenID() {
+		Message msg = new Message();
+		MessageService msgService = new MessageService();
+
+		msg.setAuthor("foo");
+		Timestamp postedAt = new Timestamp(1234);
+		msg.setPostedAt(postedAt);
+		msg.setContent("testLoadMessageInfoByGivenID");
+
+		msgService.addWallMessage("foo", msg);
+
+		long id = 1;
+
+		Message retreived = msgService.loadMessage(id);
+
+		assertTrue(retreived instanceof Message);
+
+	}
+
+	@Test
+	public void testSendPrivateMessageToAnotherUser() {
+		Message msg = new Message();
+		MessageService msgService = new MessageService();
+
+		msg.setAuthor("");
+		msg.setContent("");
+		assertNull(msgService.postPrivateChatMessage(null, null, msg));
+
+		msg.setAuthor("foo");
+		Timestamp postedAt = new Timestamp(1234);
+		msg.setPostedAt(postedAt);
+		msg.setContent("testSendPrivateMessageToAnotherUser");
+
+		Response resp = msgService.postPrivateChatMessage(user1.getUserName(), user2.getUserName(), msg);
+		assertTrue(resp instanceof Response);
 	}
 }
