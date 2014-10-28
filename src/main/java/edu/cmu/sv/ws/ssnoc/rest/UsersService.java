0495704a39369fffb5d8cc7ca26e1d61952e9d3a
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-28 00:15:19 -0700

    Add metrhod to retreive active users

--- src/main/java/edu/cmu/sv/ws/ssnoc/rest/UsersService.java
+++ src/main/java/edu/cmu/sv/ws/ssnoc/rest/UsersService.java
@@ -19,37 +19,35 @@ import edu.cmu.sv.ws.ssnoc.dto.User;
 @Path("/users")
 public class UsersService extends BaseService {
 	/**
-	 * This method loads all active users in the system.
-	 * 
-	 * @return - List of all active users.
+	 * This method loads all users in the system.
+	 *
+	 * @return - List of all users.
 	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@XmlElementWrapper(name = "users")
 	public List<User> loadUsers() {
 		Log.enter();
-
 		List<User> users = null;
 		try {
 			List<UserPO> userPOs = DAOFactory.getInstance().getUserDAO().loadUsers();
-
 			users = new ArrayList<User>();
 			for (UserPO po : userPOs) {
 				User dto = ConverterUtils.convert(po);
 				users.add(dto);
 			}
 		} catch (Exception e) {
-			handleException(e);
+			Log.error("Could not load users!");
+			this.handleException(e);
 		} finally {
 			Log.exit(users);
 		}
-
 		return users;
 	}
-	
+
 	/**
 	 * This method loads up the users those have chatted with the specified user in the past
-	 * 
+	 *
 	 * @return - array of all active users
 	 */
 	@GET
@@ -57,25 +55,47 @@ public class UsersService extends BaseService {
 	@Path("/{username}/chatbuddies")
 	public List<User> loadChatBuddies(@PathParam("username") String userName) {
 		Log.enter();
-		
 		List<User> users = null;
-		try
-		{
+		try {
 			List<UserPO> userPOs = DAOFactory.getInstance().getUserDAO().loadChatBuddies(userName);
-			
 			users = new ArrayList<User>();
 			for (UserPO po : userPOs) {
 				User dto = ConverterUtils.convert(po);
 				users.add(dto);
 			}
-			
-			
 		} catch (Exception e) {
-			
+			Log.error("Could not load chat buddies!");
+			this.handleException(e);
 		} finally {
-			
+			Log.exit();
 		}
-		
+		return users;
+	}
+
+	/**
+	 * This method loads all active users in the system
+	 *
+	 * @return - array of all active users
+	 */
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/active")
+	public List<User> loadActiveUsers() {
+		Log.enter();
+		List<User> users = this.loadUsers();
+		try
+		{
+			for (User u : users) {
+				if (u.getAccountStatus() != "active") {
+					users.remove(u);
+				}
+			}
+		} catch (Exception e) {
+			Log.error("Could not iterate users list");
+			this.handleException(e);
+		} finally {
+			Log.exit();
+		}
 		return users;
 	}
 }
