dd6aee33f2d2b224f8f0ffb52bb2896b8bd89d35
Lloyd D'Silva <lloyddsilva@gmail.com>
2014-11-12 10:56:15 -0800

    Removed unused calls

--- src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/IUserGroupDAO.java
+++ src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/IUserGroupDAO.java
@@ -8,8 +8,6 @@ import java.util.Set;
  * Interface specifying the contract that all implementations will implement to
  */
 public interface IUserGroupDAO {
-	List<String> loadUserBuddies(String userName);
-	List<String> loadUserBuddies(String userName, long timeInHours);
 	List<String> getAllUsers();
 	Map<String, Set<String>> getAllBuddies(int timeWindowInHours);
 }
--- src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/UserGroupDAOImpl.java
+++ src/main/java/edu/cmu/sv/ws/ssnoc/data/dao/UserGroupDAOImpl.java
@@ -7,7 +7,6 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -41,55 +40,6 @@ public class UserGroupDAOImpl extends BaseDAOImpl implements IUserGroupDAO {
 		} catch (SQLException e) {
 			handleException(e);
 		} finally {
-			Log.exit(userNames);
-		}
-		return userNames;
-	}
-
-	
-
-	@Override
-	public List<String> loadUserBuddies(String userName) {
-		Log.enter();
-
-		String query = SQL.FETCH_CHAT_BUDDIES_SNA;
-
-		List<String> userNames = new ArrayList<String>();
-		try (Connection conn = getConnection();
-				PreparedStatement stmt = conn.prepareStatement(query);) {
-			stmt.setString(1,  userName);
-			stmt.setString(2,  userName);
-			userNames = processUserNames(stmt);
-		} catch (SQLException e) {
-			handleException(e);
-			Log.exit(userNames);
-		}
-		
-		HashSet<String> uniqueUserNames = new HashSet<String>();
-		uniqueUserNames.addAll(userNames);
-		userNames.clear();
-		userNames.addAll(uniqueUserNames);
-		Collections.sort(userNames);
-		
-		return userNames;
-	}
-
-	@Override
-	public List<String> loadUserBuddies(String userName, long timeInHours) {
-		Log.enter();
-
-		String query = SQL.FETCH_CHAT_BUDDIES_SNA_TIME;
-
-		List<String> userNames = new ArrayList<String>();
-		try (Connection conn = getConnection();
-				PreparedStatement stmt = conn.prepareStatement(query);) {
-			stmt.setString(1,  userName);
-			stmt.setLong(2, timeInHours * -1 );
-			stmt.setString(3,  userName);
-			stmt.setLong(4, timeInHours * -1 );
-			userNames = processUserNames(stmt);
-		} catch (SQLException e) {
-			handleException(e);
 			Log.exit(userNames);
 		}
 		return userNames;
